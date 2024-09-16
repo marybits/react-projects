c1ef450736fa3d02c44ca3bd70a2ba0807e75014
marybits <marybits22@gmail.com>
2024-09-16 15:46:20 -0400

    Slider project

---
+++ 07_slider/src/App.jsx
@@ -0,0 +1,11 @@
+import Carousel from './Carousel'
+import SlickCarousel from './SlickCarousel'
+
+const App = () => {
+  return (
+    <main>
+      <Carousel />
+    </main>
+  )
+}
+export default App
